9eda073faa7d1ba0890acd77d9ba7e9ca6bd1728
ahmed-guedmioui <ahmedgire01@gmail.com>
2023-12-12 16:15:43 +0100

    added all dependacies & updated gradle

--- app/src/main/java/com/med/drawing/splash/presentaion/SplashActivity.java
+++ app/src/main/java/com/med/drawing/splash/presentaion/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.med.drawing;
+package com.med.drawing.splash.presentaion;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -6,6 +6,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+
+import com.med.drawing.AdsManager;
+import com.med.drawing.R;
+import com.med.drawing.StartActivity;
 
 
 public class SplashActivity extends Activity {
